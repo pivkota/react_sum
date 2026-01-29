3559eceb6caab92bb9b49dee0b9ef08d34c83824
Andrii Pushkar <tankist135andrei@gmail.com>
2026-01-29 13:52:25 +0200

    fixed

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -2,8 +2,6 @@ export const Sum = ({ a = 0, b = 0 }) => {
   const result = a + b;
 
   return (
-    <p>
-      Sum of {a} and {b} is {result}
-    </p>
+    <p>{`Sum of ${a} and ${b} is ${result}`}</p>
   );
 };
